857508c415389c9a9071888a7a953deef02d3f8c
maashiross <sss13masa@gmail.com>
2026-02-26 22:05:02 +0900

    Update urls.py

--- myproject/urls.py
+++ myproject/urls.py
@@ -20,7 +20,7 @@ from crud import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.TopView.as_view(), name="top"),
+    path('', views.TopView.as_view(), name="detail"),
     path('crud/', views.ProductListView.as_view(), name="list"),
     path('crud/new/', views.ProductCreateView.as_view(), name="new"),
     path('crud/edit/<int:pk>', views.ProductUpdateView.as_view(), name="edit"),
